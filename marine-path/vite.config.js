d21482ca02436fca60817e6c6616f1c5ae46da5b
Phong nè <phongpvse161146@fpt.edu.vn>
2025-10-17 14:17:10 +0700

    add

--- marine-path/vite.config.js
+++ marine-path/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 2000, // tăng giới hạn lên 2MB (mặc định là 500KB)
+  },
 })
