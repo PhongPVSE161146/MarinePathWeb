b292e6b8810175351023b33769e97a193591b515
Phong nè <phongpvse161146@fpt.edu.vn>
2025-10-17 14:29:18 +0700

    code

--- marine-path/vite.config.js
+++ marine-path/vite.config.js
@@ -1,10 +1,9 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // ✅ thêm dòng này
+  base: './', // ✅ thêm dòng này để asset load đúng trên Vercel
   build: {
     chunkSizeWarningLimit: 2000,
   },
